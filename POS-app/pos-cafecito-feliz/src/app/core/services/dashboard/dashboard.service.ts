708ddb0d23d4fa79517bb16809be987b8a188629
GusSegura <gustavo.igemer@gmail.com>
2026-02-18 22:02:09 -0600

    cambios en _redirects para corregir las rutas

--- POS-app/pos-cafecito-feliz/src/app/core/services/dashboard/dashboard.service.ts
+++ POS-app/pos-cafecito-feliz/src/app/core/services/dashboard/dashboard.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
-
-  private baseUrl = 'http://localhost:3000/api/dashboard';
+  private baseUrl = `${environment.BACK_URL}/dashboard`;
 
   constructor(private http: HttpClient) {}
 
